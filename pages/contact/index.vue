f2c3156f6df403b703c7cd3a99a262d82bf3ece4
dtpu25 <dtpu25>
2024-08-29 14:51:19 +0900

    form test1

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -23,7 +23,7 @@ const PAGE_TITLE = "ご予約・お問い合わせ";
     <div class="camera-form">
         <div class="camera-form-inner">
 
-            <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" action="/pages/success/index.vue">
+            <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" action="/pages/success">
                 <input type="hidden" name="form-name" value="contact" />
                 <div class="form-item">
                     <label for="name">お名前</label>
